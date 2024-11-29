12f551a78228ad5b1fa5e73e6457f3af5c77ec34
Akyoto1 <119670345+Akyoto1@users.noreply.github.com>
2024-11-29 16:53:03 -0500

    update1

---
+++ script.js
@@ -0,0 +1,31 @@
+// let student = {
+//   name: "Jessica",
+//   age: 25,
+//   enrolled: true,
+//   courses: [
+//     "Fundamentals of JavaScript",
+//     "Database Concepts & Design",
+//     "Operating Systems",
+//   ],
+//   greet: function () {
+//     console.log("Hello, my name is " + this.name);
+//   },
+// };
+
+console.log(student.name),
+  console.log(student.age),
+  console.log(student.enrolled),
+  console.log(student.courses);
+student.greet();
+
+let obj = {
+  name: "Sonia",
+  age: 33,
+};
+let jsonString = JSON.stringify(obj);
+console.log(jsonString);
+
+let jsonString = '{"name":"Terry","age":19}';
+let jsonObj = JSON.parse(jsonString);
+console.log(jsonObj.name);
+console.log(jsonObj.age);
