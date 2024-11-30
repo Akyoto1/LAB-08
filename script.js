33c2e80d60b1114c5af135a78114ffc27f915149
Akyoto1 <119670345+Akyoto1@users.noreply.github.com>
2024-11-29 23:04:12 -0500

    update13

--- script.js
+++ script.js
@@ -1,115 +1,115 @@
-// <<<<<<< HEAD
-// let student = {
-//   name: "Jessica",
-//   age: 25,
-//   enrolled: true,
-//   courses: [
-//     "Fundamentals of JavaScript",
-//     "Database Concepts & Design",
-//     "Operating Systems",
-//   ],
-//   greet: function () {
-//     console.log("Hello, my name is " + this.name);
-//   },
-// };
+<<<<<<< HEAD
+let student = {
+  name: "Jessica",
+  age: 25,
+  enrolled: true,
+  courses: [
+    "Fundamentals of JavaScript",
+    "Database Concepts & Design",
+    "Operating Systems",
+  ],
+  greet: function () {
+    console.log("Hello, my name is " + this.name);
+  },
+};
 
-// console.log(student.name),
-//   console.log(student.age),
-//   console.log(student.enrolled),
-//   console.log(student.courses);
-// student.greet();
+console.log(student.name),
+  console.log(student.age),
+  console.log(student.enrolled),
+  console.log(student.courses);
+student.greet();
 
-// let obj = {
-//   name: "Sonia",
-//   age: 33,
-// };
-// let jsonString = JSON.stringify(obj);
-// console.log(jsonString);
+let obj = {
+  name: "Sonia",
+  age: 33,
+};
+let jsonString = JSON.stringify(obj);
+console.log(jsonString);
 
-// let jsonString = '{"name":"Terry","age":19}';
-// let jsonObj = JSON.parse(jsonString);
-// console.log(jsonObj.name);
-// console.log(jsonObj.age);
+let jsonString = '{"name":"Terry","age":19}';
+let jsonObj = JSON.parse(jsonString);
+console.log(jsonObj.name);
+console.log(jsonObj.age);
 
-// let student = {
-//   name: "Jessica",
-//   age: 25,
-//   enrolled: true,
-//   courses: [
-//     "Fundamentals of JavaScript",
-//     "Database Concepts & Design",
-//     "Operating Systems",
-//   ],
-// };
+let student = {
+  name: "Jessica",
+  age: 25,
+  enrolled: true,
+  courses: [
+    "Fundamentals of JavaScript",
+    "Database Concepts & Design",
+    "Operating Systems",
+  ],
+};
 
-// let { name, courses } = student;
-// console.log(student.name);
-// console.log(student.courses);
-// =======
-// let student = {
-//   name: "Jessica",
-//   age: 25,
-//   enrolled: true,
-//   courses: [
-//     "Fundamentals of JavaScript",
-//     "Database Concepts & Design",
-//     "Operating Systems",
-//   ],
-//   greet: function () {
-//     console.log("Hello, my name is " + this.name);
-//   },
-// };
-// >>>>>>> 811dac7c8beaa9db220b255bded27eb8ad89846c
+let { name, courses } = student;
+console.log(student.name);
+console.log(student.courses);
+=======
+let student = {
+  name: "Jessica",
+  age: 25,
+  enrolled: true,
+  courses: [
+    "Fundamentals of JavaScript",
+    "Database Concepts & Design",
+    "Operating Systems",
+  ],
+  greet: function () {
+    console.log("Hello, my name is " + this.name);
+  },
+};
+>>>>>>> 811dac7c8beaa9db220b255bded27eb8ad89846c
 
-// let grades = [80, 85, 90];
-// let [first, second] = grades;
+let grades = [80, 85, 90];
+let [first, second] = grades;
 
-// console.log(first);
-// console.log(second);
+console.log(first);
+console.log(second);
 
-// let originalStudent = {
-//   name: "Jessica",
-//   age: 25,
-//   enrolled: true,
-//   courses: [
-//     "Fundamentals of JavaScript",
-//     "Database Concepts & Design",
-//     "Operating Systems",
-//   ],
-// };
+let originalStudent = {
+  name: "Jessica",
+  age: 25,
+  enrolled: true,
+  courses: [
+    "Fundamentals of JavaScript",
+    "Database Concepts & Design",
+    "Operating Systems",
+  ],
+};
 
-// let student = { originalStudent };
-// let clonedStudent = { ...originalStudent, graduationYear: 2027 };
+let student = { originalStudent };
+let clonedStudent = { ...originalStudent, graduationYear: 2027 };
 
-// console.log(clonedStudent);
-// let studentCourses = [
-//   "Fundamentals of JavaScript",
-//   "Database Concepts & Design",
-//   "Operating Systems",
-// ];
-// let newCourses = [
-//   "Algorithms and Problem Solving",
+console.log(clonedStudent);
+let studentCourses = [
+  "Fundamentals of JavaScript",
+  "Database Concepts & Design",
+  "Operating Systems",
+];
+let newCourses = [
+  "Algorithms and Problem Solving",
 
-//   "Technical Reading and Writing Skills",
-// ];
+  "Technical Reading and Writing Skills",
+];
 
-// let combinedCourses = [...studentCourses, ...newCourses];
-// console.log(combinedCourses);
+let combinedCourses = [...studentCourses, ...newCourses];
+console.log(combinedCourses);
 
-// let student = {
-//     name: "Jessica",
-//     courses: [
-//       "Fundamentals of JavaScript",
-//       "Database Concepts & Design",
-//       "Operating Systems",
-//       addCourses = "Web Programming and Design",
-//        this.courses.push("Web Programming and Design"),
-//     ]
-// student.addCourses("Web Programming and Design"),
-//  };
-// console.log(student.courses);
+let student = {
+    name: "Jessica",
+    courses: [
+      "Fundamentals of JavaScript",
+      "Database Concepts & Design",
+      "Operating Systems",
+      addCourses = "Web Programming and Design",
+       this.courses.push("Web Programming and Design"),
+    ]
+student.addCourses("Web Programming and Design"),
+ };
+console.log(student.courses);
 
-// function sum(80, 90, 78) {
-//     return SVGAnimatedNumberList.reduce((total, num) => total + num, 0);
-// }
-// console.log(sum(80, 90, 78));
+function sum(80, 90, 78) {
+    return SVGAnimatedNumberList.reduce((total, num) => total + num, 0);
+}
+console.log(sum(80, 90, 78));
